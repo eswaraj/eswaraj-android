069ea126a31e49091905c3e025e761b0698c849f
vaibhav-sinha <vaibhavsinh@gmail.com>
2015-02-01 16:41:11 +0530

    Added icon for location

--- app/src/main/java/com/eswaraj/app/eswaraj/adapters/GlobalSearchAdapter.java
+++ app/src/main/java/com/eswaraj/app/eswaraj/adapters/GlobalSearchAdapter.java
@@ -57,10 +57,10 @@ public class GlobalSearchAdapter extends ArrayAdapter<GlobalSearchResponseDto> {
         if(globalSearchResponseDto.getType().equals("Location")) {
             holder.sDetails.setText(globalSearchResponseDto.getSubType());
             if(globalSearchResponseDto.getImage() != null && !globalSearchResponseDto.getImage().equals("")) {
-                Picasso.with(context).load(globalSearchResponseDto.getImage().replace("http", "https")).error(R.drawable.anon_grey).placeholder(R.drawable.anon_grey).into(holder.sImage);
+                Picasso.with(context).load(globalSearchResponseDto.getImage().replace("http", "https")).error(R.drawable.anon_loc_grey).placeholder(R.drawable.anon_loc_grey).into(holder.sImage);
             }
             else {
-                holder.sImage.setImageDrawable(context.getResources().getDrawable(R.drawable.anon_grey));
+                holder.sImage.setImageDrawable(context.getResources().getDrawable(R.drawable.anon_loc_grey));
             }
         }
         else if(globalSearchResponseDto.getType().equals("Leader")) {
