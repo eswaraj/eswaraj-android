83e44ca0573a52f645dda74e6646d099cb4fb1c9
vaibhav-sinha <vaibhavsinh@gmail.com>
2015-02-03 23:46:04 +0530

    MyLocationButton moved to bottom

--- app/src/main/java/com/eswaraj/app/eswaraj/fragments/GoogleMapFragment.java
+++ app/src/main/java/com/eswaraj/app/eswaraj/fragments/GoogleMapFragment.java
@@ -2,6 +2,10 @@ package com.eswaraj.app.eswaraj.fragments;
 
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 
 import com.eswaraj.app.eswaraj.application.EswarajApplication;
@@ -66,6 +70,22 @@ public class GoogleMapFragment extends SupportMapFragment implements OnMapReadyC
     public void setContext(OnMapReadyCallback context) {
         this.callback = context;
         super.getMapAsync(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View mapView = super.onCreateView(inflater, container, savedInstanceState);
+
+        // Get the button view
+        View locationButton = ((View) mapView.findViewById(1).getParent()).findViewById(2);
+        // and next place it, for example, on bottom right (as Google Maps app)
+        RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+        // position on right bottom
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        rlp.setMargins(0, 0, 30, 30);
+
+        return mapView;
     }
 
     @Override
