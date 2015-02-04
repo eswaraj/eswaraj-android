58f3a90a82dc247b57a83731df1d49b105ce08f6
vaibhav-sinha <vaibhavsinh@gmail.com>
2015-02-05 02:51:08 +0530

    Location check

--- app/src/main/java/com/eswaraj/app/eswaraj/util/LocationServicesCheckUtil.java
+++ app/src/main/java/com/eswaraj/app/eswaraj/util/LocationServicesCheckUtil.java
@@ -1,7 +1,12 @@
 package com.eswaraj.app.eswaraj.util;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.provider.Settings;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -9,7 +14,8 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 public class LocationServicesCheckUtil {
 
     public Boolean isServiceAvailable(Context context) {
-        return isLocationServiceAvailable(context);
+        //return isLocationServiceAvailable(context);
+        return isAnyProviderAvailable(context);
     }
 
     private Boolean isLocationServiceAvailable(Context context) {
@@ -18,5 +24,50 @@ public class LocationServicesCheckUtil {
             return true;
         }
         return false;
+    }
+
+    private Boolean isAnyProviderAvailable(final Context context) {
+        Boolean available = false;
+        LocationManager lm = null;
+        boolean gps_enabled = false, network_enabled = false;
+
+        if(lm == null) {
+            lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        }
+
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch(Exception ex) {}
+
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch(Exception ex) {}
+
+
+        if(!gps_enabled && !network_enabled) {
+            return false;
+            /*
+            AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+            dialog.setMessage("Enable Location Service");
+            dialog.setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    Intent myIntent = new Intent( Settings.ACTION_SECURITY_SETTINGS );
+                    context.startActivity(myIntent);
+                    //get gps
+                }
+            });
+            dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    return;
+                }
+            });
+            dialog.show();
+            */
+
+        }
+
+        return true;
     }
 }
